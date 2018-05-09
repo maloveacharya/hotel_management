d34e189460e15079d5384f36dc1e894bb6362ba2
adarsh-patel <pateladarsh14@gmail.com>
2018-05-09 17:18:22 +0530

    [Update] update a wizard

--- hotel_management/wizard/calc.py
+++ hotel_management/wizard/calc.py
@@ -1,20 +1,34 @@
-from odoo import api,fields,models
+from odoo import api,fields,models,_
 
 class sample(models.Model):
     _name= 'sample.sample'
 
     @api.multi
     def Calc_data(self):
-        ans = 0
+
         if self.select == 'sum':
-            ans = self.a + self.b
-            print ('\n\n',ans,'\n\n')
+            self.ans = self.a + self.b
+            print ('\n\n',self.ans,'\n\n')
         elif self.select == 'minus':
-            ans= self.a-self.b
-            print ('\n\n', ans, '\n\n')
+            self.ans= self.a-self.b
+            print ('\n\n', self.ans, '\n\n')
+        ctx = dict(self.env.context)
+        ctx.update({'default_ans': self.ans})
+        print ("\n\n\n-------ctx---------->",ctx)
+        return {
+            'name': _('Display ans'),
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'sample.sample',
+            'view_id': self.env.ref('hotel_management.sample_form_another_view').id,
+            'context':ctx,
+            'type': 'ir.actions.act_window',
+            'target': 'new'
+        }
+
 
     a = fields.Float('A')
     b = fields.Float('B')
     select = fields.Selection([('sum','Sum'),('minus','Subtraction')])
-
+    ans = fields.Float('Ans',readonly=True)
 
